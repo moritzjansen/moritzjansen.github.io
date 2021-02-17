92c370fa20273430763f91037d3f61da8ef9f321
Moritz Jansen <mojansen95@gmail.com>
2021-02-17 12:29:25 +0100

    iss funcitonality

---
+++ scripts/iss.js
@@ -0,0 +1,13 @@
+// main(); 
+
+// function main(){
+//     let textToEdit = document.querySelector("#word");
+//     count = 0; 
+//     wordsArray = ["cool", "amazing", "super"]; 
+//     setInterval(function () { 
+//         count++; 
+//         textToEdit.fadeOut(500, function () { 
+//         $(this).text(wordsArray[count % wordsArray.length]).fadeIn(500); 
+//         }); 
+//     }, 2000); 
+// }
