0ed233dc313a99f4d420dc11fdf23012b7a7f620
Moritz Jansen <mojansen95@gmail.com>
2023-01-16 11:52:17 +0100

    Add darker BG color for content

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ function IndexPage(){
 
   return (
   <Layout title="Home">
-    <div className='flex flex-col mx-auto items-start justify-start h-full pt-10 gap-3 max-w-3xl px-10'>
+    <div className='flex flex-col mx-auto items-start justify-start h-full pt-10 gap-3 max-w-3xl px-10 shadow-2xl bg-[#00000044]'>
       <img className=' self-center rounded-full border-white border-8 shadow-2xl transition-transform hover:scale-110' src="/images/fog.jpg" alt="me in the fog" width="256" height="256" onMouseOver={hover} onMouseLeave={unhover}></img>
       <h1 className='text-5xl text-white'>Hi there! 👋</h1>
       <p className='text-white text-lg'>
-        My name is Mo, I just finished my masters degree in Computer Science at RWTH Aachen University and this is my website (WIP)!
+        My name is Mo and this is my website (WIP)!
       </p>
       <p className='text-white text-lg'>
         For my latest projects check out my <a target="_blank" rel="noopener noreferrer" className='underline' href='https://github.com/moritzjansen'>GitHub</a> page.
